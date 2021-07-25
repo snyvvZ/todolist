8612febdf253c74c0b9a3d1590e30d75bc1455fc
김신영/프론트개발팀 <ksy@musinsa.com>
2021-07-25 11:40:35 +0900

    템플릿 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import GlobalStyle from "./styles/GlobalStyle";
+import TodoTemplate from "./components/TodoTemplate";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <div>Hello</div>
-      <div>React</div>
+      <TodoTemplate>하이하이</TodoTemplate>
     </>
   );
 };
