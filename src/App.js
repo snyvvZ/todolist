3cf1833262225b6ab40ade201dc7fb13c7ef15af
김신영/프론트개발팀 <ksy@musinsa.com>
2021-07-25 11:58:23 +0900

    요소 추가하기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GlobalStyle from "./styles/GlobalStyle";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoHeader from "./components/TodoHeader";
 import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <TodoTemplate>
         <TodoHeader />
         <TodoList />
+        <TodoCreate />
       </TodoTemplate>
     </>
   );
