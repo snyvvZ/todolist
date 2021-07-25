22c981e6d2bfe2a5b995fa623bb1be5f4e03b6c2
김신영/프론트개발팀 <ksy@musinsa.com>
2021-07-25 15:33:40 +0900

    잘 안되고 뭐가 뭔지 모르겠다 더 공부하고 오기

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
 
 const StyledHeader = styled.section`
   padding-top: 48px;
@@ -26,11 +27,23 @@ const StyledHeader = styled.section`
 `;
 
 const TodoHeader = () => {
+  const todos = useTodoState();
+  const undoneTasks = todos.filter((todo) => !todo.done);
+  console.log(1);
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
+
   return (
     <StyledHeader>
-      <h1>2019년 7월 10일</h1>
-      <div className="day">수요일</div>
-      <div className="tasks-left">할 일 2개 남음</div>
+      <h1>{dateString}</h1>
+      <div className="day">{dayName}</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
     </StyledHeader>
   );
 };
