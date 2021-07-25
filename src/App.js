ab23265c8896aad9d8760940200c02deb16ef84e
김신영/프론트개발팀 <ksy@musinsa.com>
2021-07-25 15:24:26 +0900

    TodoContext 생성

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoHeader from "./components/TodoHeader";
 import TodoList from "./components/TodoList";
 import TodoCreate from "./components/TodoCreate";
+import { TodoProvider } from "./TodoContext";
 
 const App = () => {
   return (
     <>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHeader />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
+      <TodoProvider>
+        <GlobalStyle />
+        <TodoTemplate>
+          <TodoHeader />
+          <TodoList />
+          <TodoCreate />
+        </TodoTemplate>
+      </TodoProvider>
     </>
   );
 };
