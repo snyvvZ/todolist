c3171693e9f340102d1e15fe71d66bc728620b30
김신영/프론트엔드개발팀 <ksy@musinsa.com>
2021-08-07 13:03:09 +0900

    다시 시작

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import React from "react";
-import GlobalStyle from "./styles/GlobalStyle";
-import TodoTemplate from "./components/TodoTemplate";
-import TodoHeader from "./components/TodoHeader";
-import TodoList from "./components/TodoList";
-import TodoCreate from "./components/TodoCreate";
-import { TodoProvider } from "./TodoContext";
 
 const App = () => {
   return (
     <>
-      <TodoProvider>
-        <GlobalStyle />
-        <TodoTemplate>
-          <TodoHeader />
-          <TodoList />
-          <TodoCreate />
-        </TodoTemplate>
-      </TodoProvider>
     </>
   );
 };
